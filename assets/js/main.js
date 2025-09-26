d027c9cc967af1891ca4fea2dba2f03a0250b8bf
BAYSM2026 <baysm2026@gmail.com>
2025-09-26 11:52:14 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,7 @@ var spy = new Gumshoe('#navigation a', {
 
 /* ======= Countdown ========= */
 // set the date we're counting down to
-var target_date = new Date("Apr 7, 2025").getTime();
+var target_date = new Date("June 16, 2026 09:00:00").getTime();
  
 // variables for time units
 var days, hours, minutes, seconds;
